2da03812c27d1006be9501aced6ab30624a29f2c
Beirao <thomasmarques911@gmail.com>
2024-05-28 19:05:45 +0200

    LZ tests setup.

--- deploy/CdxUSD.ts
+++ deploy/CdxUSD.ts
@@ -36,8 +36,8 @@ const deploy: DeployFunction = async (hre) => {
     const { address } = await deploy(contractName, {
         from: deployer,
         args: [
-            'MyOFT', // name
-            'MOFT', // symbol
+            'Cod3x USD', // name
+            'cdxUSD', // symbol
             endpointV2Deployment.address, // LayerZero's EndpointV2 address
             deployer, // owner
         ],
--- test/mocks/OFTMock.sol
+++ test/mocks/OFTMock.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import { CdxUSD } from "contracts/CdxUSD.sol";
 import { SendParam } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTCore.sol";
 
-contract OFTMock is OFT {
+contract OFTMock is CdxUSD {
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
-    ) Ownable(_delegate) OFT(_name, _symbol, _lzEndpoint, _delegate) {}
+    ) CdxUSD(_name, _symbol, _lzEndpoint, _delegate) {}
 
     function mint(address _to, uint256 _amount) public {
         _mint(_to, _amount);
