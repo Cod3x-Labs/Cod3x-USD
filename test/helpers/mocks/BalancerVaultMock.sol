9a315d070c5dfa95edf1d168a58f3ebbaab211a7
Beirao <thomasmarques911@gmail.com>
2024-08-13 11:40:06 +0200

    fmt

--- test/helpers/mocks/BalancerVaultMock.sol
+++ test/helpers/mocks/BalancerVaultMock.sol
@@ -6,47 +6,60 @@ import "forge-std/console.sol";
 import {IVault} from "../../../contracts/staking_module/vault_strategy/interfaces/IVault.sol";
 
 contract BalancerVaultMock {
-    
     mapping(bytes32 => mapping(address => uint256)) _idToTokenAndCash;
     mapping(bytes32 => IERC20[]) _idToTokens;
 
-    constructor(bytes32 poolId, address[] memory tokens, uint256[] memory balances){
+    constructor(bytes32 poolId, address[] memory tokens, uint256[] memory balances) {
         _setBalancesForTokens(poolId, tokens, balances);
     }
 
-    function setBalancesForTokens(bytes32 poolId, address[] memory tokens, uint256[] memory balances) external {
+    function setBalancesForTokens(
+        bytes32 poolId,
+        address[] memory tokens,
+        uint256[] memory balances
+    ) external {
         _setBalancesForTokens(poolId, tokens, balances);
     }
 
-
-    function _setBalancesForTokens(bytes32 poolId, address[] memory tokens, uint256[] memory balances) internal {
+    function _setBalancesForTokens(
+        bytes32 poolId,
+        address[] memory tokens,
+        uint256[] memory balances
+    ) internal {
         delete _idToTokens[poolId];
-        for(uint8 idx_ = 0; idx_ < tokens.length; idx_++){
+        for (uint8 idx_ = 0; idx_ < tokens.length; idx_++) {
             _idToTokenAndCash[poolId][tokens[idx_]] = balances[idx_];
             _idToTokens[poolId].push(IERC20(tokens[idx_]));
             // console.log("Pushing: ", _idToTokens[poolId][idx_]);
         }
     }
 
-    function getPoolTokenInfo(bytes32 poolId, IERC20 token) external view
-        returns (uint256 cash, uint256 managed, uint256 blockNumber, address assetManager){
-            return (_idToTokenAndCash[poolId][address(token)], 0, 0, address(0));
+    function getPoolTokenInfo(bytes32 poolId, IERC20 token)
+        external
+        view
+        returns (uint256 cash, uint256 managed, uint256 blockNumber, address assetManager)
+    {
+        return (_idToTokenAndCash[poolId][address(token)], 0, 0, address(0));
     }
 
-    function getPoolTokens(bytes32 poolId) external view returns(IERC20[] memory tokens, uint256[] memory balances, uint256 lastChangeBlock) {
+    function getPoolTokens(bytes32 poolId)
+        external
+        view
+        returns (IERC20[] memory tokens, uint256[] memory balances, uint256 lastChangeBlock)
+    {
         tokens = _idToTokens[poolId];
         balances = new uint256[](3);
         lastChangeBlock = 0;
-        for(uint8 i = 0; i < tokens.length; i++){
+        for (uint8 i = 0; i < tokens.length; i++) {
             balances[i] = _idToTokenAndCash[poolId][address(tokens[i])];
         }
     }
 
-    function getPool(bytes32 poolId) external view returns(address, IVault.PoolSpecialization){
+    function getPool(bytes32 poolId) external view returns (address, IVault.PoolSpecialization) {
         return (address(this), IVault.PoolSpecialization.GENERAL);
     }
 
-    function getBptIndex() external view returns(uint256){
+    function getBptIndex() external view returns (uint256) {
         return 2;
     }
 }
