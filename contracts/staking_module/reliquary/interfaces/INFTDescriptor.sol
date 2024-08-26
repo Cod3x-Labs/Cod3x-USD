a92d9ebe7b4ae38908adbfc784cf70acddad2a84
CergyK <cergyk.eth@gmail.com>
2024-08-26 17:16:22 +0200

    remove contracts

--- contracts/staking_module/reliquary/interfaces/INFTDescriptor.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
-
-interface INFTDescriptor {
-    function constructTokenURI(uint256 _relicId) external view returns (string memory);
-}
--- contracts/staking_module/reliquary/interfaces/IParentRollingRewarder.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
-
-import "./IRewarder.sol";
-
-interface IParentRollingRewarder is IRewarder {
-    function initialize(uint8 _poolId) external;
-}
