2beff35819206c0e61ed733541e634f3638169b7
Beirao <thomasmarques911@gmail.com>
2025-03-14 10:24:53 +0100

    ReliquaryBalancerGaugeV3 fixed

--- test/foundry/staking_module/reliquary/ReliquaryBalancerGaugeV3.t.sol
+++ test/foundry/staking_module/reliquary/ReliquaryBalancerGaugeV3.t.sol
@@ -424,36 +424,38 @@ contract ReliquaryBalancerGaugeV3 is ERC721Holder, Test {
         assertEq(reliquary.getPositionForId(newRelicId).amount, depositAmount1 + depositAmount2);
     }
 
-    // // TODO
-    // function testCompareDepositAndMerge(uint256 amount1, uint256 amount2, uint256 time) public {
-    //     amount1 = bound(amount1, 1e4, IERC20(balancerPool).balanceOf(address(this)) - 1);
-    //     amount2 = bound(amount2, 1, IERC20(balancerPool).balanceOf(address(this)) - amount1);
-    //     time = bound(time, 1 weeks, 20 weeks);
+    function testCompareDepositAndMerge(uint256 amount1, uint256 amount2, uint256 time) public {
+        amount1 = bound(amount1, 1e18, IERC20(balancerPool).balanceOf(address(this)) - 1e18);
+        amount2 = bound(amount2, 1e18, IERC20(balancerPool).balanceOf(address(this)) - amount1);
+        time = bound(time, 1 weeks, 20 weeks);
 
-    //     uint256 relicId = reliquary.createRelicAndDeposit(address(this), 0, amount1);
-    //     skip(time);
-    //     reliquary.deposit(amount2, relicId, address(0));
-    //     uint256 maturity1 = block.timestamp - reliquary.getPositionForId(relicId).entry;
+        uint256 relicId = reliquary.createRelicAndDeposit(address(this), 0, amount1);
+        skip(time);
+        reliquary.deposit(amount2, relicId, address(0));
+        uint256 maturity1 = block.timestamp - reliquary.getPositionForId(relicId).entry;
 
-    //     // Reset maturity
-    //     reliquary.withdraw(amount1 + amount2, relicId, address(0));
-    //     reliquary.deposit(amount1, relicId, address(0));
+        // Reset maturity
+        reliquary.withdraw(amount1 + amount2, relicId, address(0));
+        reliquary.deposit(amount1, relicId, address(0));
 
-    //     // Gauge claim
-    //     uint256 treasuryBalanceBefore = IERC20(wmatic).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertGt(IERC20(wmatic).balanceOf(treasury), treasuryBalanceBefore);
-    //     uint256 treasuryBalanceMid = IERC20(wmatic).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertEq(IERC20(wmatic).balanceOf(treasury), treasuryBalanceMid);
+        // Gauge claim
+        uint256 treasuryBalanceBeforeGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceBeforeBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBeforeGho);
+        assertGt(IERC20(balToken).balanceOf(treasury), treasuryBalanceBeforeBal);
+        uint256 treasuryBalanceMidGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceMidBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMidGho);
+        assertEq(IERC20(balToken).balanceOf(treasury), treasuryBalanceMidBal);
+        skip(time);
+        uint256 newRelicId = reliquary.createRelicAndDeposit(address(this), 0, amount2);
+        reliquary.merge(newRelicId, relicId);
+        uint256 maturity2 = block.timestamp - reliquary.getPositionForId(relicId).entry;
 
-    //     skip(time);
-    //     uint256 newRelicId = reliquary.createRelicAndDeposit(address(this), 0, amount2);
-    //     reliquary.merge(newRelicId, relicId);
-    //     uint256 maturity2 = block.timestamp - reliquary.getPositionForId(relicId).entry;
-
-    //     assertApproxEqAbs(maturity1, maturity2, 1);
-    // }
+        assertApproxEqAbs(maturity1, maturity2, 1);
+    }
 
     function testMergeAfterSplit() public {
         uint256 depositAmount1 = 100 ether;
@@ -526,145 +528,158 @@ contract ReliquaryBalancerGaugeV3 is ERC721Holder, Test {
         reliquary.createRelicAndDeposit(address(this), 0, 1000);
     }
 
-    // // TODO
-    // function testRelic1Level() public {
-    //     uint256 relic1 = 1;
+    function testRelic1Level() public {
+        uint256 relic1 = 1;
 
-    //     reliquary.deposit(999, 1, address(0));
-    //     vm.stopPrank();
-    //     uint256 relic2 = reliquary.createRelicAndDeposit(address(this), 0, 1000);
+        reliquary.deposit(999, 1, address(0));
+        uint256 relic2 = reliquary.createRelicAndDeposit(address(this), 0, 1e10);
 
-    //     assertEq(reliquary.getPositionForId(relic1).level, 0);
-    //     assertEq(reliquary.getPositionForId(relic2).level, 0);
+        assertEq(reliquary.getPositionForId(relic1).level, 0);
+        assertEq(reliquary.getPositionForId(relic2).level, 0);
 
-    //     skip(1 days);
+        skip(1 days);
 
-    //     reliquary.update(relic1, address(0));
-    //     reliquary.update(relic2, address(0));
+        reliquary.update(relic1, address(0));
+        reliquary.update(relic2, address(0));
 
-    //     assertEq(reliquary.getPositionForId(relic1).level, 0);
-    //     assertGt(reliquary.getPositionForId(relic2).level, 0);
+        assertEq(reliquary.getPositionForId(relic1).level, 0);
+        assertGt(reliquary.getPositionForId(relic2).level, 0);
 
-    //     skip(1 days);
+        skip(1 days);
 
-    //     reliquary.update(relic1, address(0));
-    //     reliquary.update(relic2, address(0));
+        reliquary.update(relic1, address(0));
+        reliquary.update(relic2, address(0));
 
-    //     assertEq(reliquary.getPositionForId(relic1).level, 0);
-    //     assertGt(reliquary.getPositionForId(relic2).level, 0);
+        assertEq(reliquary.getPositionForId(relic1).level, 0);
+        assertGt(reliquary.getPositionForId(relic2).level, 0);
 
-    //     // Gauge claim
-    //     uint256 treasuryBalanceBefore = IERC20(wmatic).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertGt(IERC20(wmatic).balanceOf(treasury), treasuryBalanceBefore);
-    //     uint256 treasuryBalanceMid = IERC20(wmatic).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertEq(IERC20(wmatic).balanceOf(treasury), treasuryBalanceMid);
-    // }
+        // Gauge claim
+        uint256 treasuryBalanceBeforeGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceBeforeBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBeforeGho);
+        assertGt(IERC20(balToken).balanceOf(treasury), treasuryBalanceBeforeBal);
+        uint256 treasuryBalanceMidGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceMidBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMidGho);
+        assertEq(IERC20(balToken).balanceOf(treasury), treasuryBalanceMidBal);
+    }
 
-    // function testRelic1RewardDistribution1(uint256 seedTime) public {
-    //     uint256 time = bound(seedTime, 1 days, 365 days);
+    function testRelic1RewardDistribution1(uint256 seedTime) public {
+        uint256 time = bound(seedTime, 1 days, 365 days);
 
-    //     uint256 relic1 = 1;
+        uint256 relic1 = 1;
 
-    //     reliquary.deposit(999, 1, address(0));
-    //     vm.stopPrank();
-    //     uint256 relic2 = reliquary.createRelicAndDeposit(address(this), 0, 1000);
+        reliquary.deposit(999, 1, address(0));
+        vm.stopPrank();
+        uint256 relic2 = reliquary.createRelicAndDeposit(address(this), 0, 1e10);
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic1, address(11));
-    //     reliquary.update(relic2, address(22));
+        reliquary.update(relic1, address(11));
+        reliquary.update(relic2, address(22));
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic1, address(11));
-    //     reliquary.update(relic2, address(22));
+        reliquary.update(relic1, address(11));
+        reliquary.update(relic2, address(22));
 
-    //     // test relic 1 linearity
-    //     assertGt(oath.balanceOf(address(22)), oath.balanceOf(address(11)));
+        // test relic 1 linearity
+        assertGt(oath.balanceOf(address(22)), oath.balanceOf(address(11)));
 
-    //     // Gauge claim
-    //     uint256 treasuryBalanceBeforeGho = IERC20(gho).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBeforeGho);
-    //     uint256 treasuryBalanceMidGho = IERC20(gho).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMidGho);
-    // }
+        // Gauge claim
+        uint256 treasuryBalanceBeforeGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceBeforeBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBeforeGho);
+        assertGt(IERC20(balToken).balanceOf(treasury), treasuryBalanceBeforeBal);
+        uint256 treasuryBalanceMidGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceMidBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMidGho);
+        assertEq(IERC20(balToken).balanceOf(treasury), treasuryBalanceMidBal);
+    }
 
-    // function testRelic1RewardDistribution2(uint256 seedTime) public {
-    //     uint256 time = bound(seedTime, 1 days, 365 days);
+    function testRelic1RewardDistribution2(uint256 seedTime) public {
+        uint256 time = bound(seedTime, 1 days, 10 days);
 
-    //     uint256 relic1 = 1;
+        uint256 relic1 = 1;
 
-    //     reliquary.deposit(999, 1, address(0));
-    //     reliquary.createRelicAndDeposit(address(this), 0, 1000);
+        reliquary.deposit(999, 1, address(0));
+        reliquary.createRelicAndDeposit(address(this), 0, 1e10);
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic1, address(11));
+        reliquary.update(relic1, address(11));
 
-    //     uint256 balance11 = oath.balanceOf(address(11));
+        uint256 balance11 = oath.balanceOf(address(11));
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic1, address(11));
+        reliquary.update(relic1, address(11));
 
-    //     // test relic 1 linearity
-    //     assertApproxEqRel(oath.balanceOf(address(11)), balance11 * 2, 1e2); // 0
+        // test relic 1 linearity
+        assertApproxEqRel(oath.balanceOf(address(11)), balance11 * 2, 1e3); // 0
 
-    //     skip(time);
+        skip(time);
 
-    //     // Gauge claim
-    //     uint256 treasuryBalanceBefore = IERC20(gho).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBefore);
-    //     uint256 treasuryBalanceMid = IERC20(gho).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMid);
+        // Gauge claim
+        uint256 treasuryBalanceBeforeGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceBeforeBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBeforeGho);
+        assertGt(IERC20(balToken).balanceOf(treasury), treasuryBalanceBeforeBal);
+        uint256 treasuryBalanceMidGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceMidBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMidGho);
+        assertEq(IERC20(balToken).balanceOf(treasury), treasuryBalanceMidBal);
 
-    //     reliquary.update(relic1, address(11));
+        reliquary.update(relic1, address(11));
 
-    //     // test relic 1 linearity
-    //     assertApproxEqRel(oath.balanceOf(address(11)), balance11 * 3, 1e2); // 0
-    // }
+        // test relic 1 linearity
+        assertApproxEqRel(oath.balanceOf(address(11)), balance11 * 3, 1e3); // 0
+    }
 
-    // // TODO
-    // function testRelic2ToNRewardDistribution() public {
-    //     uint256 time = 365 days; // bound(seedTime, 1, 365 days);
+    function testRelic2ToNRewardDistribution() public {
+        uint256 time = 365 days; // bound(seedTime, 1, 365 days);
 
-    //     reliquary.deposit(999, 1, address(0));
-    //     uint256 relic2 = reliquary.createRelicAndDeposit(address(this), 0, 1000);
+        reliquary.deposit(999, 1, address(0));
+        uint256 relic2 = reliquary.createRelicAndDeposit(address(this), 0, 1e10);
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic2, address(22));
+        reliquary.update(relic2, address(22));
 
-    //     uint256 balance22 = oath.balanceOf(address(22));
+        uint256 balance22 = oath.balanceOf(address(22));
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic2, address(22));
+        reliquary.update(relic2, address(22));
 
-    //     // Gauge claim
-    //     uint256 treasuryBalanceBefore = IERC20(gho).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBefore);
-    //     uint256 treasuryBalanceMid = IERC20(gho).balanceOf(treasury);
-    //     reliquary.claimRehypothecation(0);
-    //     assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMid);
+        // Gauge claim
+        uint256 treasuryBalanceBeforeGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceBeforeBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertGt(IERC20(gho).balanceOf(treasury), treasuryBalanceBeforeGho);
+        assertGt(IERC20(balToken).balanceOf(treasury), treasuryBalanceBeforeBal);
+        uint256 treasuryBalanceMidGho = IERC20(gho).balanceOf(treasury);
+        uint256 treasuryBalanceMidBal = IERC20(balToken).balanceOf(treasury);
+        reliquary.claimRehypothecation(0);
+        assertEq(IERC20(gho).balanceOf(treasury), treasuryBalanceMidGho);
+        assertEq(IERC20(balToken).balanceOf(treasury), treasuryBalanceMidBal);
 
-    //     // test relic 1 linearity
-    //     assertGt(oath.balanceOf(address(22)), balance22 * 2); // 0
+        // test relic 1 linearity
+        assertGt(oath.balanceOf(address(22)), balance22 * 2); // 0
 
-    //     skip(time);
+        skip(time);
 
-    //     reliquary.update(relic2, address(22));
+        reliquary.update(relic2, address(22));
 
-    //     // test relic 1 linearity
-    //     assertGt(oath.balanceOf(address(22)), balance22 * 3); // 0
-    // }
+        // test relic 1 linearity
+        assertGt(oath.balanceOf(address(22)), balance22 * 3); // 0
+    }
 
     function testRelic1ProhibitedActions() public {
         uint256 relic1 = 1;
